1923087864fb6725f9dfe721580096aa5e575a90
aminul <rahathssn@gmail.com>
2024-09-09 15:49:05 +0600

    added 27-5 Absolute path, relative path, outlet, and Layout layer.

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -1,9 +1,11 @@
+import { Outlet } from "react-router-dom";
 
 
 const AdminLayout = () => {
     return (
         <div>
-            <h2>This is Admin Layout</h2>
+            <h2>Navbar in AdminLayout</h2>
+            <Outlet/>
         </div>
     );
 };
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            <h2>This is Login component</h2>
+        </div>
+    );
+};
+
+export default Login;

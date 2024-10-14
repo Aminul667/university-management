54039d69ca652601d0bd442846ddbfb5c0cd2ba2
aminul <rahathssn@gmail.com>
2024-10-14 19:28:15 +0600

    added setting the next for user management

---
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -0,0 +1,10 @@
+
+const CreateAcademicFaculty = () => {
+    return (
+        <div>
+            <h2>This is CreateAcademicFaculty component</h2>
+        </div>
+    );
+};
+
+export default CreateAcademicFaculty;

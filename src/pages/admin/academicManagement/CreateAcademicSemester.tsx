e6d9d8671a89977b6dba799ff7905d96bb3202c6
aminul <rahathssn@gmail.com>
2024-10-15 00:25:31 +0600

    added reusable select element

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -6,16 +6,41 @@ import PHSelect from "../../../components/form/PHSelect";
 
 const CreateAcademicSemester = () => {
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const semesterData = {
+      name: "Something",
+      code: "Something",
+    };
     console.log(data);
+    console.log(semesterData);
   };
+
+  // 'Autumn',
+  // 'Summer',
+  // 'Fall',
+
+  const nameOptions = [
+    {
+      value: "01",
+      label: "Autumn",
+    },
+    {
+      value: "02",
+      label: "Summer",
+    },
+    {
+      value: "03",
+      label: "Fall",
+    },
+  ];
 
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-          <PHInput type="text" name="name" label="name" />
-          <PHInput type="text" name="name" label="year" />
-          <PHSelect label="name" />
+          {/* <PHInput type="text" name="name" label="name" /> */}
+          {/* <PHInput type="text" name="name" label="year" /> */}
+          <PHSelect label="Name" name="name" options={nameOptions} />
+          <PHSelect label="Code" name="code" options={nameOptions} />
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
